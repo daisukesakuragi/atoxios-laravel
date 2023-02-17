a6595d0b82dd644865cbad7516c6a41035a3d325
takuboy93 <wacky34581@gmail.com>
2023-02-17 13:45:35 +0900

    Disable admin registration routes

--- routes/admin.php
+++ routes/admin.php
@@ -17,8 +17,8 @@ use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:admin')->group(function () {
-    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('/register', [RegisteredUserController::class, 'store']);
+    // Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    // Route::post('/register', [RegisteredUserController::class, 'store']);
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
     Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
