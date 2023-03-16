db153da0ecf7d2d71f6daf042338c475af9a11ee
takuboy93 <wacky34581@gmail.com>
2023-03-16 18:53:08 +0900

    Apply daysi ui table to plan detail page

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -2,6 +2,7 @@
     <section class="tw-pt-24 tw-pb-16">
         <div class="tw-container tw-max-w-screen-xl">
             <x-page-title title="あなたの入札履歴" subtitle="YOUR BID HISTORY"></x-page-title>
+            @if(count($bids) > 0)
             <div class="tw-overflow-x-auto tw-w-full">
                 <table class="tw-table tw-w-full tw-mb-12">
                     <thead>
@@ -13,7 +14,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($bids as $bid)
+                        @foreach ($bids as $bid)
                         <tr>
                             <td>
                                 {{ mb_strlen($bid->plan->title) > 15 ? mb_substr($bid->plan->title, 0, 15) . '...' : $bid->plan->title }}
@@ -28,18 +29,19 @@
                                 <button class="tw-btn tw-btn-primary tw-btn-sm">詳細はこちら</button>
                             </th>
                         </tr>
-                        @empty
-                        <div class="tw-alert tw-shadow-lg tw-max-w-2xl tw-mx-auto">
-                            <div>
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="tw-stroke-info tw-flex-shrink-0 tw-w-6 tw-h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                              <span class="tw-font-bold tw-text-sm">{{ __('入札履歴のデータが見つかりませんでした。') }}</span>
-                            </div>
-                        </div>
-                        @endforelse
+                        @endforeach
                     </tbody>                  
                 </table>
             </div>
             {{ $bids->links() }}
+            @else
+            <div class="tw-alert tw-shadow-lg tw-max-w-2xl tw-mx-auto">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="tw-stroke-info tw-flex-shrink-0 tw-w-6 tw-h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  <span class="tw-font-bold tw-text-sm">{{ __('入札履歴のデータが見つかりませんでした。') }}</span>
+                </div>
+            </div>
+            @endif
         </div>
     </section>
 </x-app-layout>
--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -6,16 +6,8 @@
         <div class="tw-hero-overlay tw-bg-opacity-60 tw-pt-32 tw-pb-24"></div>
         <div class="tw-hero-content tw-text-center tw-text-neutral-content tw-pt-32 tw-pb-24">
             <div class="tw-max-w-screen-sm tw-mx-auto">
-                <h1 class="tw-mb-2 tw-text-5xl lg:tw-text-7xl tw-font-bold tw-italic">
-                    {{ $plan->title }}
-                </h1>
-                <div class="tw-flex tw-items-center tw-justify-end tw-gap-2 tw-mb-8">
-                    <span>{{ __('by') }}</span>
-                    <a href="{{ route('members.show', $plan->member->slug)}}" class="tw-underline tw-font-bold">
-                        {{ $plan->member->name }}
-                    </a>
-                </div>
-                <x-bid-button :plan="$plan"></x-bid-button>
+                <h1 class="tw-mb-2 tw-text-5xl lg:tw-text-7xl tw-font-bold tw-italic">{{ $plan->title }}</h1>
+                <p class="tw-text-right">{{ __('by') }}<a href="{{ route('members.show', $plan->member->slug)}}" class="tw-underline tw-font-bold">{{ $plan->member->name }}</a></p>
             </div>
         </div>
     </section>
@@ -59,40 +51,48 @@
     <section class="tw-py-12">
         <div class="tw-container tw-max-w-screen-sm">
             <x-section-title title="入札履歴" subtitle="BID HISTORY"></x-section-title>
-            <div class="tw-flex tw-flex-col tw-gap-y-6 tw-mb-8">
-                @forelse ($bids as $bid)
-                @if(auth()->check() && $bid->user->id === auth()->user()->id)
-                <div class="tw-bg-white tw-rounded-lg tw-shadow-lg tw-border-2 tw-border-primary">
-                    <div class="tw-bg-white tw-rounded-lg tw-shadow-lg">
-                        <div class="tw-p-6">
-                            <h3 class="tw-text-lg tw-font-semibold">
-                                {{ $bid->price }}円
-                            </h3>
-                            <p class="tw-text-sm tw-text-gray-700">{{ $bid->created_at }}</p>
-                        </div>
-                    </div>
-                </div>
-                @else
-                <div class="tw-bg-white tw-rounded-lg tw-shadow-lg">
-                    <div class="tw-bg-white tw-rounded-lg tw-shadow-lg">
-                        <div class="tw-p-6">
-                            <h3 class="tw-text-lg tw-font-semibold">
-                                {{ $bid->price }}円
-                            </h3>
-                            <p class="tw-text-sm tw-text-gray-700">{{ $bid->created_at }}</p>
-                        </div>
-                    </div>
-                </div>
-                @endif
-                @empty
-                <div class="tw-alert tw-shadow-lg tw-max-w-2xl tw-mx-auto">
-                    <div>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="tw-stroke-info tw-flex-shrink-0 tw-w-6 tw-h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                      <span class="tw-font-bold tw-text-sm">{{ __('入札履歴が見つかりませんでした。') }}</span>
-                    </div>
-                </div>
-                @endforelse
+            @if(count($bids) > 0)
+            <div class="tw-overflow-x-auto tw-w-full">
+                <table class="tw-table tw-w-full tw-mb-12">
+                    <thead>
+                        <tr>
+                            <th>{{ __('入札金額') }}</th>
+                            <th>{{ __('入札日時') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($bids as $bid)
+                        @if(auth()->check() && $bid->user->id === auth()->user()->id)
+                        <tr>
+                            <td>
+                                <strong class="tw-mr-2">{{ number_format($bid->price) . '円' }}</strong><span class="tw-badge">{{ __('あなたの入札') }}</span>
+                            </td>
+                            <td>
+                                {{ date('Y/m/d H:i:s', strtotime($bid->created_at)) }}
+                            </td>
+                        </tr>
+                        @else
+                        <tr>
+                            <td>
+                                {{ number_format($bid->price) . '円' }}
+                            </td>
+                            <td>
+                                {{ date('Y/m/d H:i:s', strtotime($bid->created_at)) }}
+                            </td>
+                        </tr>
+                        @endif
+                        @endforeach
+                    </tbody>                  
+                </table>
             </div>
+            @else
+            <div class="tw-alert tw-shadow-lg tw-max-w-2xl tw-mx-auto">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="tw-stroke-info tw-flex-shrink-0 tw-w-6 tw-h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  <span class="tw-font-bold tw-text-sm">{{ __('入札履歴のデータが見つかりませんでした。') }}</span>
+                </div>
+            </div>
+            @endif
             <x-bid-button :plan="$plan"></x-bid-button>
         </div>
     </section>
